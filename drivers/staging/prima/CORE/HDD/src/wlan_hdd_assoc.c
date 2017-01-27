d06b68f0e3825c4043abad263fc6ded677517131
Hriday Sharma <hridaysharma42@gmail.com>
2017-01-27 11:40:29 +0000

    prima: HDD: properly send NL80211_ATTR_DISCONNECTED_BY_AP in disconnect
    This commit https://github.com/HridayHS/android_kernel_xiaomi_msm8937/commit/4c1c7d26cd1f16a7a8439edefe5aea55939de39d Introduced new changes for cfg80211_disconnected function,
    So, update cfg80211_disconnected functions in PRIMA: HDD according to new changes
    
    Signed-off-by: Hriday Sharma <hridaysharma42@gmail.com>

--- drivers/staging/prima/CORE/HDD/src/wlan_hdd_assoc.c
+++ drivers/staging/prima/CORE/HDD/src/wlan_hdd_assoc.c
@@ -1089,11 +1089,11 @@ static eHalStatus hdd_DisConnectHandler( hdd_adapter_t *pAdapter, tCsrRoamInfo *
                /* To avoid wpa_supplicant sending "HANGED" CMD to ICS UI */
                if ( eCSR_ROAM_LOSTLINK == roamStatus )
                {
-                   cfg80211_disconnected(dev, pRoamInfo->reasonCode, NULL, 0, GFP_KERNEL);
+                   cfg80211_disconnected(dev, pRoamInfo->reasonCode, NULL, 0, false, GFP_KERNEL);
                }
                else
                {
-                   cfg80211_disconnected(dev, WLAN_REASON_UNSPECIFIED, NULL, 0, GFP_KERNEL);
+                   cfg80211_disconnected(dev, WLAN_REASON_UNSPECIFIED, NULL, 0, false, GFP_KERNEL);
                }
             }
 
