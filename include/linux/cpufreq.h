e8cf60ad8121c16f8e15f84ee8d9218cece77154
Luís Ferreira <lsferreira169@gmail.com>
2017-01-29 18:51:14 +0000

    Revert "Add interactivex CPU Governor"
    This reverts commit ba785d733bad86640d506c66759f6bb4e9bd2cee.

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -500,9 +500,6 @@ extern struct cpufreq_governor cpufreq_gov_conservative;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_INTERACTIVE)
 extern struct cpufreq_governor cpufreq_gov_interactive;
 #define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_interactive)
-#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_INTERACTIVEX)
-extern struct cpufreq_governor cpufreq_gov_interactivex;
-#define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_interactivex)
 #endif
 
 /*********************************************************************
